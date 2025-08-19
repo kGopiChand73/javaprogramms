01f344e7fd40e0559660b947733aeeec862aa73c
kGopiChand73 <kgopichand699@gmail.com>
2025-08-19 14:25:10 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {5, 3, 8, 6, 2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("Sorted array (Selection Sort):");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
